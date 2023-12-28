3d91bf3c5634e8f50e463185f14f2319faafd112
GeordzNZ <geordie.king@gmail.com>
2023-12-28 19:07:02 +0000

    Add project files.

---
+++ SampleFileExplorer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SampleFileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFileType = new System.Windows.Forms.Label();
+            this.lstvDisplay = new System.Windows.Forms.ListView();
+            this.lstIcons = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(15, 13);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(31, 23);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "<--";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(740, 13);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(48, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(52, 15);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(682, 20);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Filename:";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(92, 411);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(16, 13);
+            this.lblFileName.TabIndex = 5;
+            this.lblFileName.Text = "- -";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(611, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File Type:";
+            // 
+            // lblFileType
+            // 
+            this.lblFileType.AutoSize = true;
+            this.lblFileType.Location = new System.Drawing.Point(672, 411);
+            this.lblFileType.Name = "lblFileType";
+            this.lblFileType.Size = new System.Drawing.Size(16, 13);
+            this.lblFileType.TabIndex = 7;
+            this.lblFileType.Text = "- -";
+            // 
+            // lstvDisplay
+            // 
+            this.lstvDisplay.HideSelection = false;
+            this.lstvDisplay.LargeImageList = this.lstIcons;
+            this.lstvDisplay.Location = new System.Drawing.Point(15, 41);
+            this.lstvDisplay.Name = "lstvDisplay";
+            this.lstvDisplay.Size = new System.Drawing.Size(773, 266);
+            this.lstvDisplay.SmallImageList = this.lstIcons;
+            this.lstvDisplay.TabIndex = 8;
+            this.lstvDisplay.UseCompatibleStateImageBehavior = false;
+            this.lstvDisplay.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstvDisplay_ItemSelectionChanged);
+            // 
+            // lstIcons
+            // 
+            this.lstIcons.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.lstIcons.ImageSize = new System.Drawing.Size(48, 48);
+            this.lstIcons.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 450);
+            this.Controls.Add(this.lstvDisplay);
+            this.Controls.Add(this.lblFileType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFileType;
+        private System.Windows.Forms.ListView lstvDisplay;
+        private System.Windows.Forms.ImageList lstIcons;
+    }
+}
+
---
+++ SampleFileExplorer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleFileExplorer
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "D:";
+        private bool isFile = false;
+        private string currentlySelectedItemName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtFilePath.Text = filePath;
+            loadFilesAndDirectories();
+        }
+
+        public void loadFilesAndDirectories() 
+        {
+            DirectoryInfo fileList;
+            try
+            {
+                fileList = new DirectoryInfo(filePath);
+                FileInfo[] files = fileList.GetFiles();  //GETS ALL THE FILES
+                DirectoryInfo[] dirs = fileList.GetDirectories();  // GETS ALL THE DIRECTORIES
+
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    lstvDisplay.Items.Add(files[i].Name);
+                }
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    lstvDisplay.Items.Add(files[i].Name);
+                }
+
+            }
+            catch(Exception e)
+            {
+            
+            }
+        }
+
+        public void loadButtonAction()
+        {
+            filePath = txtFilePath.Text;
+            loadFilesAndDirectories();
+            isFile = false;
+        }
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void lstvDisplay_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            currentlySelectedItemName = e.Item.Text;
+        }
+    }
+}
